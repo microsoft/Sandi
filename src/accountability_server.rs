8db58af65700a6250bf9407d988995d4ae7fa3b3
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2023-10-06 13:51:17 -0700

    Partial work

--- src/accountability_server.rs
+++ src/accountability_server.rs
@@ -1,8 +1,11 @@
 use rand::{RngCore, CryptoRng};
+use chrono::{ Utc, Duration };
+use ed25519_dalek::{ SigningKey, Signature, Signer };
 use crate::{sender_ids::get_sender, utils::encrypt};
 
 pub struct AccountabilityServer {
-    secret_key: [u8; 32],
+    enc_secret_key: [u8; 32],
+    signing_key: SigningKey,
 }
 
 impl AccountabilityServer {
@@ -10,21 +13,30 @@ impl AccountabilityServer {
     where
         R: RngCore + CryptoRng,
     {
-        let mut secret_key = [0u8; 32];
-        rng.fill_bytes(&mut secret_key);
+        let mut enc_secret_key = [0u8; 32];
+        rng.fill_bytes(&mut enc_secret_key);
 
-        AccountabilityServer { secret_key }
+        let signing_key = SigningKey::generate(rng);
+
+        AccountabilityServer { enc_secret_key, signing_key }
     }
 
-    pub fn issue_tag(&self, commitment: Vec<u8>, sender_handle: &str) {
+    pub fn issue_tag(&self, commitment: Vec<u8>, sender_handle: &str, tag_duration: u32) {
         // First, we need to check if the sender_handle is valid
         let sender = get_sender(sender_handle).expect("Sender handle not found");
 
         // Then, we encrypt the sender ID
         let mut encrypted_sender_id = sender.id.clone();
-        encrypt(&self.secret_key, &mut encrypted_sender_id);
+        encrypt(&self.enc_secret_key, &mut encrypted_sender_id);
+
+        // Get expiration date for the tag
+        // tag_duration is in hours
+        let expiration_date = Utc::now().timestamp() + Duration::hours(tag_duration as i64).num_seconds();
 
         // Then, we sign tag information
-        
+        let mut data_to_sign = Vec::new();
+        data_to_sign.extend(commitment);
+        data_to_sign.extend_from_slice(expiration_date.to_be_bytes().as_slice());
+        data_to_sign.extend_from_slice(&encrypted_sender_id);
     }
 }
