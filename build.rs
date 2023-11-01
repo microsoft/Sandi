32760f3a21429f7721ed2b1f5ef0ff9b8fbaf7cd
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2023-11-01 11:50:07 -0700

    Call flatc from build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,17 @@
+use std::process::Command;
 
 fn main()
 {
-    println!("cargo:rerun-if-changed={}", "fb/tag.fbs");
-    println!("cargo:rerun-if-changed={}", "fb/full_tag.fbs");
-    println!("cargo:rerun-if-changed={}", "fb/common.fbs");
+    println!("cargo:rerun-if-changed=fb/tag.fbs");
+    println!("cargo:rerun-if-changed=fb/full_tag.fbs");
+    println!("cargo:rerun-if-changed=fb/common.fbs");
+
+    let fc = flatc::flatc();
+    let output = Command::new(fc)
+        .args(&["-o", "src/serialization", "--rust", "fb/tag.fbs", "fb/full_tag.fbs"])
+        .output()
+        .expect("failed to execute process");
+    if !output.status.success() {
+        panic!("flatc failed: {}", String::from_utf8_lossy(&output.stderr));
+    }
 }
